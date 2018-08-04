8ea1651071752a92f5fd18f3a2571a9462d30606
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-08-04 16:21:48 -0700

    organizing mixpanel

---
+++ mixpanelTracking.js
@@ -0,0 +1,20 @@
+function websiteVisit(){
+    mixpanel.track("personal website");
+}
+function trackTwitterButton(){
+    mixpanel.track("Clicked Twitter link");
+}
+function trackResumePdfButton(){
+    mixpanel.track("Clicked Resume PDF link");
+}
+function trackGithubButton(){
+    mixpanel.track("Clicked Github link");
+}
+function trackLinkedInButton(){
+    mixpanel.track("Clicked LinkedIn link");
+}
+function trackOnlineResumeButton(){
+    mixpanel.track("Clicked OnlineResume link");
+}
+       
+  

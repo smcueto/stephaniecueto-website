8f2161abe710d6595bb1f7b466728d34f98d60c9
Stephanie Cueto <stephaniem.cueto@gmail.com>
2018-10-13 09:41:33 -0700

    adding token

--- mixpanelLibrary.js
+++ mixpanelLibrary.js
@@ -11,4 +11,4 @@
         }; a.__SV = 1.2; b = e.createElement("script"); b.type = "text/javascript"; b.async = !0; b.src = "undefined" !== typeof MIXPANEL_CUSTOM_LIB_URL ? MIXPANEL_CUSTOM_LIB_URL : "file:" === e.location.protocol && "//cdn4.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//) ? "https://cdn4.mxpnl.com/libs/mixpanel-2-latest.min.js" : "//cdn4.mxpnl.com/libs/mixpanel-2-latest.min.js"; c = e.getElementsByTagName("script")[0]; c.parentNode.insertBefore(b, c)
     }
 })(document, window.mixpanel || []);
-mixpanel.init(token.mixpanelToken);
+mixpanel.init('ace14ad1e26c75f3b4468289ebd37215');
